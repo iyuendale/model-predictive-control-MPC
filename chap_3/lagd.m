8c5db8b58754e65d03b06563045f3af84ac004a5
Iyuel Endale <iyuelendale@gmail.com>
2023-01-09 15:39:16 +0300

    Update lagd.m
    description abut the function

--- chap_3/lagd.m
+++ chap_3/lagd.m
@@ -1,4 +1,5 @@
-function [A, L0] = lagd(a, N)
+% creates the Laguerre network matrix A_l and initial vector L0
+function [A_l, L0] = lagd(a, N)
 v(1, 1) = a; L0(1, 1) = 1;
 for k = 2:N
 	v(k, 1) = (-a).^(k-2)*(1-a*a);
@@ -6,7 +7,7 @@ for k = 2:N
 end
 
 L0 = sqrt((1-a*a))*L0;
-A(:, 1) = v;
+A_l(:, 1) = v;
 for i = 2:N
-	A(:, i) = [zeros(i-1, 1); v(1: N-i+1, 1)];
+	A_l(:, i) = [zeros(i-1, 1); v(1: N-i+1, 1)];
 end

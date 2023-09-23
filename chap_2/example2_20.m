ce214dd3318880a36218ae45bcf393fd97503b39
Iyuel Endale <iyuelendale@gmail.com>
2023-09-23 23:55:50 +0300

    update example 2.20

--- chap_2/example2_20.m
+++ chap_2/example2_20.m
@@ -60,38 +60,39 @@ k2 = buf2(:, 1); u = buf2(:, 2); xm = [0 0]';
 subplot 311, plot(k, y), hold on % , axis([0 60, 0 1.5]), hold on
 subplot 312, plot(k, deltaU), hold on % , axis([0 60 -2 7]), hold on
 subplot 313, plot(k, U), hold on % , axis([0 60 -6 15]), hold on
-%% without constraints
-xm= [0 0 0]'; xm_old= xm; x= [0 0 0 0]'; buf = []; u = 0; buf2= [];
+% %% without constraints
+% xm= [0 0 0]'; xm_old= xm; x= [0 0 0 0]'; buf = []; u = 0; buf2= [];
+% 
+% for k = 1:19
+% 	FF= phi'*(F*x - Rs);
+% 	deltaU= -E\FF;
+% 	u = u+deltaU(1);
+% 	xm = am*xm + bm*u;
+% 	x = [xm - xm_old; cm*xm];
+% 	buf = [buf; k, cm*xm deltaU(1) u];
+% 	buf2= [buf2; [k k+1]' [u u]'];
+% end
+% 
+% for k = 20:39
+% 	FF= phi'*(F*x - Rs);
+% 	deltaU= -E\FF;
+% 	u = u+deltaU(1);
+% 	xm = am*xm + bm*u + [0 0 1]';
+% 	x = [xm - xm_old; cm*xm];
+% 	buf = [buf; k, cm*xm deltaU(1) u];
+% 	buf2= [buf2; [k k+1]' [u u]'];
+% end
+% 
+% for k = 40:60
+% 	FF= phi'*(F*x - Rs);
+% 	deltaU= -E\FF;
+% 	u = u+deltaU(1);
+% 	xm = am*xm + bm*u - [0 0 1]';
+% 	x = [xm - xm_old; cm*xm];
+% 	buf = [buf; k, cm*xm deltaU(1) u];
+% 	buf2= [buf2; [k k+1]' [u u]'];
+% end
 
-for k = 1:19
-	FF= phi'*(F*x - Rs);
-	deltaU= -E\FF;
-	u = u+deltaU(1);
-	xm = am*xm + bm*u;
-	x = [xm - xm_old; cm*xm];
-	buf = [buf; k, cm*xm deltaU(1) u];
-	buf2= [buf2; [k k+1]' [u u]'];
-end
-
-for k = 20:39
-	FF= phi'*(F*x - Rs);
-	deltaU= -E\FF;
-	u = u+deltaU(1);
-	xm = am*xm + bm*u + [0 0 1]';
-	x = [xm - xm_old; cm*xm];
-	buf = [buf; k, cm*xm deltaU(1) u];
-	buf2= [buf2; [k k+1]' [u u]'];
-end
-
-for k = 40:60
-	FF= phi'*(F*x - Rs);
-	deltaU= -E\FF;
-	u = u+deltaU(1);
-	xm = am*xm + bm*u - [0 0 1]';
-	x = [xm - xm_old; cm*xm];
-	buf = [buf; k, cm*xm deltaU(1) u];
-	buf2= [buf2; [k k+1]' [u u]'];
-end
 k = buf(:, 1); y = buf(:, 2); deltaU = buf(:, 3);
 k2 = buf2(:, 1); u = buf2(:, 2);
 subplot 311, plot(k, y), axis([0 60, 0 1.5]), title Output

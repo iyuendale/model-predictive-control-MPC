babd00fa64779c062a26e7b32f6cb1b4973bdcb1
Iyuel Endale <iyuelendale@gmail.com>
2023-10-01 18:12:37 +0300

    Update MPC gains calculations

--- chap_1/IEmpcgains2.m
+++ chap_1/IEmpcgains2.m
@@ -2,7 +2,9 @@ function [F,phi,RRs] = IEmpcgains2(A,B,C,Np,Nc,rs)
 %  for augmented system
 RRs = rs*ones(Np,1);
 phi = zeros(Np, Nc);
+F = [];
 for a = 1:Np
+    F = [F; C*A^a];
     for b = 1:Nc
         if a < b
             phi(a,b) = 0;
@@ -10,8 +12,4 @@ for a = 1:Np
             phi(a,b) = C*A^(a-b)*B;
         end
     end
-end
-F = [];
-for a = 1:Np
-    F = [F; C*A^a];
 end
